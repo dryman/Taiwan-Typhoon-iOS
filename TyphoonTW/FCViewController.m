8c50dda1490c0704e3a864bc28190d5a0bae59e0
dryman <idryman@gmail.com>
2012-08-23 14:12:22 +0800

    typhoon with historical track

--- TyphoonTW/FCViewController.m
+++ TyphoonTW/FCViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "FCViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface FCViewController ()
 
@@ -76,14 +77,45 @@
     NSString *json = [ty_infos substringWithRange:NSMakeRange(range_of_match.location, range_of_match.length-1)];
     NSArray* arr = [NSJSONSerialization JSONObjectWithData:[json dataUsingEncoding:NSUTF8StringEncoding] options:0 error:nil];
     
-    for (NSDictionary *weather in arr) {
-        for (NSDictionary* fcst in [weather valueForKey:@"fcst"]) {
-            MKCircle *cir = [MKCircle circleWithCenterCoordinate:CLLocationCoordinate2DMake([[fcst valueForKey:@"lat"] floatValue], [[fcst valueForKey:@"lon"] floatValue]) radius:[[fcst valueForKey:@"pr70"]floatValue]*1000];
-            [self.mapView addOverlay:cir];
+    @autoreleasepool {
+        for (NSDictionary *typhoon in arr) {
+            size_t length = [[typhoon valueForKey:@"fcst"] count] + [[typhoon valueForKey:@"best_track"] count];
+            CLLocationCoordinate2D *line_points = (CLLocationCoordinate2D*) malloc(length*sizeof(CLLocationCoordinate2D));
+
+            int i=0;
+            for (NSDictionary *track in [typhoon valueForKey:@"best_track"]) {
+                CGFloat lat = [[track valueForKey:@"lat"] floatValue];
+                CGFloat lon = [[track valueForKey:@"lon"] floatValue];
+                CLLocationCoordinate2D location = CLLocationCoordinate2DMake(lat, lon);
+                line_points[i++] = location;
+                MKPointAnnotation *centerPoint = [[MKPointAnnotation alloc] init];
+                centerPoint.coordinate = location;
+                [self.mapView addAnnotation:centerPoint];
+            }
+            for (NSDictionary* fcst in [typhoon valueForKey:@"fcst"]) {
+                CGFloat lat = [[fcst valueForKey:@"lat"]  floatValue];
+                CGFloat lon = [[fcst valueForKey:@"lon"]  floatValue];
+                CGFloat rad = [[fcst valueForKey:@"pr70"] floatValue];
+                CLLocationCoordinate2D location = CLLocationCoordinate2DMake(lat, lon);
+                line_points[i++]=location;
+                
+                MKCircle *cir = [MKCircle circleWithCenterCoordinate: location
+                                                              radius: rad*1000];
+                [self.mapView addOverlay:cir];
+                MKPointAnnotation *centerPoint = [[MKPointAnnotation alloc] init];
+                centerPoint.coordinate = location;
+                [self.mapView addAnnotation:centerPoint];
+            }
+
+            MKPolyline *line = [MKPolyline polylineWithCoordinates:line_points count:length];
+            [self.mapView addOverlay:line];
+            free(line_points);
         }
     }
 }
-- (void) connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+
+- (void) connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
     connectionData = nil;
 }
 
@@ -95,10 +127,26 @@
         view.fillColor = [[UIColor redColor] colorWithAlphaComponent:0.05];
         view.strokeColor = [[UIColor redColor] colorWithAlphaComponent:0.25];
         view.lineWidth = 3;
-        
+        return view;
+    } else if ([overlay isKindOfClass:[MKPolyline class]]) {
+        MKPolylineView *view = [[MKPolylineView alloc] initWithPolyline:overlay];
+        view.strokeColor = [[UIColor blackColor] colorWithAlphaComponent:0.2];
+        view.lineWidth = 2;
         return view;
     }
     return nil;
+}
+- (MKAnnotationView*)mapView:(MKMapView *)mView viewForAnnotation:(id<MKAnnotation>)annotation
+{
+    NSString *identifier = @"pinView";
+    MKAnnotationView *annotationView = (MKAnnotationView*)[mView dequeueReusableAnnotationViewWithIdentifier:identifier];
+    if (annotationView==nil) {
+        annotationView = [[MKAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:identifier];
+        UIImage *img = [UIImage imageNamed:@"typh.png"];
+        annotationView.image = img;
+        annotationView.canShowCallout = NO;
+    }
+    return annotationView;
 }
 
 @end
